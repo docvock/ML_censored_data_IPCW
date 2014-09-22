8f48e7cf429b4ace0252d4115a5e3a8b3fa11240
julianw <julianw@umn.edu>
2014-09-22 11:28:10 -0500

    Initial commit

---
+++ code/IPCW-fns.R
@@ -0,0 +1,145 @@
+library(rpart)
+
+IPCW_rpart <- function(T,C,varnms,tau,train.dat,test.dat=NULL,...) {  
+  sf <- survfit(Surv(T,C)~1)  
+  sf.C <- survfit(Surv(T,1-C)~1)
+  
+  KM <- function(t) {
+    sf$surv[min(which(sf$time > t))]
+  }
+  
+  KM.C <- function(t) {
+    sf.C$surv[min(which(sf.C$time > t))]
+  }
+  
+  wts <- sapply(1:length(T),function(j) {
+    if((T[j] < (tau) & C[j]==0) ) { return(0) }
+    else { return(1/KM.C(min(T[j],tau)))}
+  })
+  
+  eventsurv.ind <- as.factor(C==1 & T <= tau)
+  fmla <- as.formula(paste0("eventsurv.ind~",paste0(varnms,collapse="+")))
+  RP <- rpart(fmla,data=train.dat,weights = wts,method="class",control=rpart.control(...))
+  
+  if(is.null(test.dat)) {
+    probs <- predict(RP,type="prob",na.action=na.omit)[,1]    
+  } else {
+    probs <- predict(RP,newdata=test.dat,type="prob",na.action=na.omit)[,1]
+  }
+  return(probs)
+}
+
+## Decision tree setting observations censored prior to tau to zero
+ZERO_rpart <- function(T,C,varnms,tau,train.dat,test.dat=NULL,...) {  
+
+  eventsurv.ind <- as.factor(C==1 & T <= tau)
+  fmla <- as.formula(paste0("eventsurv.ind~",paste0(varnms,collapse="+")))
+  RP <- rpart(fmla,data=train.dat,method="class",control=rpart.control(...))
+  
+  if(is.null(test.dat)) {
+    probs <- predict(RP,type="prob",na.action=na.omit)[,1]    
+  } else {
+    probs <- predict(RP,newdata=test.dat,type="prob",na.action=na.omit)[,1]
+  }
+  return(probs)
+}
+
+## Decision tree discarding all "unknown" results
+DISCARD_rpart <- function(T,C,varnms,tau,train.dat,test.dat=NULL,...) {  
+  
+  eventsurv.ind <- as.factor(C==1 & T <= tau)
+
+  train.dat$eventsurv <- eventsurv.ind
+  fmla <- as.formula(paste0("eventsurv~",paste0(varnms,collapse="+")))
+  RP <- rpart(fmla,data=subset(train.dat,!(T<=tau & C==0)),method="class",control=rpart.control(...))
+  
+  if(is.null(test.dat)) {
+    probs <- predict(RP,type="prob",na.action=na.omit)[,1]    
+  } else {
+    probs <- predict(RP,newdata=test.dat,type="prob",na.action=na.omit)[,1]
+  }
+  return(probs)
+}
+
+## IPCW logisitc regression
+IPCW_logistic <- function(T,C,varnms,tau,train.dat,test.dat=NULL,...) {  
+  sf <- survfit(Surv(T,C)~1)  
+  sf.C <- survfit(Surv(T,1-C)~1)
+  
+  KM <- function(t) {
+    sf$surv[min(which(sf$time > t))]
+  }
+  
+  KM.C <- function(t) {
+    sf.C$surv[min(which(sf.C$time > t))]
+  }
+  
+  wts <- sapply(1:length(T),function(j) {
+    if((T[j] < (tau) & C[j]==0) ) { return(0) }
+    else { return(1/KM.C(min(T[j],tau)))}
+  })
+  
+  eventsurv.ind <- as.integer(C==1 & T <= tau)
+  fmla <- as.formula(paste0("eventsurv.ind~",paste0(varnms,collapse="+")))
+  
+  LR <- glm(fmla,data=train.dat,weights=wts,family="binomial")
+  
+  if(is.null(test.dat)) {
+    probs <- predict(LR,type="response",na.action=na.omit)    
+  } else {
+    probs <- predict(LR,newdata=test.dat,type="response",na.action=na.omit)    
+  }
+  return(1-probs)
+}
+
+## Logistic regression setting censored prior to tau to zero
+ZERO_logistic <- function(T,C,varnms,tau,train.dat,test.dat=NULL,...) {  
+
+  eventsurv.ind <- as.integer(C==1 & T <= tau)
+  fmla <- as.formula(paste0("eventsurv.ind~",paste0(varnms,collapse="+")))
+  
+  LR <- glm(fmla,data=train.dat,family="binomial")
+  
+  if(is.null(test.dat)) {
+    probs <- predict(LR,type="response",na.action=na.omit)    
+  } else {
+    probs <- predict(LR,newdata=test.dat,type="response",na.action=na.omit)    
+  }
+  return(1-probs)
+}
+
+## Logistic regression discarding censored prior to tau
+DISCARD_logistic <- function(T,C,varnms,tau,train.dat,test.dat=NULL,...) {  
+  
+  eventsurv.ind <- as.factor(C==1 & T <= tau)
+  
+  train.dat$eventsurv <- eventsurv.ind
+  fmla <- as.formula(paste0("eventsurv~",paste0(varnms,collapse="+")))
+  
+  LR <- glm(fmla,data=subset(train.dat,!(T<=tau & C==0)),family="binomial")
+  
+  if(is.null(test.dat)) {
+    probs <- predict(LR,type="response",na.action=na.omit)    
+  } else {
+    probs <- predict(LR,newdata=test.dat,type="response",na.action=na.omit)    
+  }
+  return(1-probs)
+}
+
+library(survMisc)
+
+## Define the calibration statistic
+calib.stat <- function(p,cutpts,t,T.test,C.test) {
+  risk.class <- cut(p,cutpts,labels=FALSE)
+  lev.stats <- sapply(1:(length(cutpts)-1),function(f) {
+    ind <- which(risk.class==f)
+    S.KM <- calcSurv(Surv(T.test[ind],C.test[ind]))
+    ind.surv <- max(which(S.KM$t<=t))
+    p.KM <- S.KM$SKM[ind.surv]
+    ##print(c(cutpts[f],p.KM,mean(p[ind],na.rm=TRUE),sqrt(S.KM$SKMV[ind.surv])))
+    (mean(p[ind],na.rm=TRUE) - p.KM)^2/S.KM$SKMV[ind.surv]
+  })
+  
+  sum(lev.stats,na.rm=TRUE)
+}
+
---
+++ code/IPCW-rpart-logistic.R
@@ -0,0 +1,57 @@
+############################
+#### Fit the HP dataset ####
+############################
+
+code.base <- "C:/Users/Julian/Google Drive/iPredict/manuscripts/ML for censored data/code/"
+dat.base <- "H:/Carlson/Portal/Data/DataLock/DataLockv2.3.1/"
+source(paste0(code.base,"IPCW-fns.R"))
+
+library(data.table)
+## Read the data
+#HP <- fread(paste0(dat.base,"Data_ver-2.3.1_updated.csv"),verbose=TRUE)
+HP.imp <- fread(paste0(dat.base,"Data_ver-2.3.1_imputed.csv"),verbose=TRUE)
+HP <- subset(HP.imp,age>=40&Comorbidity_All==0) ## Only those over age 40 without comorbidities
+
+frac.train <- 0.75
+train.set <- sample(1:nrow(HP),frac.train*nrow(HP),replace=FALSE)
+test.set <- setdiff(1:nrow(HP),train.set)
+
+HP.train <- data.frame(HP[train.set,])
+HP.test <- data.frame(HP[test.set,])
+
+## Fit the models
+SURVTIME <- 7
+
+T.train <- HP.train$DaysToEvent_Fram
+C.train <- HP.train$CVDEvent_Fram
+
+varnms <- c("gender","age","SBP","BMI","HDL","TC","Has_Diab","Smoking")
+##varnms <- c("age","gender","SBP","BMI","HDL","TC") ## Reasonably well-calibrated
+
+library(peperr)
+
+T.test <- HP.test$DaysToEvent_Fram
+C.test <- HP.test$CVDEvent_Fram
+
+SURVTIME <- 7 ## Time horizon for prediction in years
+
+pRP <- IPCW_rpart(T.train,C.train,varnms,SURVTIME*365,HP.train,HP.test,cp=0.00001,minbucket=100)
+pRP.zero <- ZERO_rpart(T.train,C.train,varnms,SURVTIME*365,HP.train,HP.test,cp=0.00001,minbucket=100)
+pRP.disc <- DISCARD_rpart(T.train,C.train,varnms,SURVTIME*365,HP.train,HP.test,cp=0.00001,minbucket=100)
+
+intx.varnms <- c(varnms,paste0("gender:",varnms[-1]))
+
+pLR <- IPCW_logistic(T.train,C.train,intx.varnms,SURVTIME*365,HP.train,HP.test)
+pLR.zero <- ZERO_logistic(T.train,C.train,intx.varnms,SURVTIME*365,HP.train,HP.test)
+pLR.disc <- DISCARD_logistic(T.train,C.train,intx.varnms,SURVTIME*365,HP.train,HP.test)
+
+risk.cutpts = c(Inf,1-c(0.05,0.1,0.15,0.2),-Inf)
+
+calib.stat(pRP,risk.cutpts,SURVTIME*365,T.test,C.test)
+calib.stat(pRP.zero,risk.cutpts,SURVTIME*365,T.test,C.test)
+calib.stat(pRP.disc,risk.cutpts,SURVTIME*365,T.test,C.test)
+calib.stat(pLR,risk.cutpts,SURVTIME*365,T.test,C.test)
+calib.stat(pLR.zero,risk.cutpts,SURVTIME*365,T.test,C.test)
+calib.stat(pLR.disc,risk.cutpts,SURVTIME*365,T.test,C.test)
+
+## Evaluate the reclassification performance of well-calibrated models
