1ad5e0af028f66472a0c1e520ef89b03b352462d
julianw <julianw@umn.edu>
2014-09-24 14:24:12 -0500

    Minor update
    Add option to print the calibration table

--- code/calibration.R
+++ code/calibration.R
@@ -1,12 +1,14 @@
 library(survMisc)
-calib.stat <- function(p,cutpts,t) {
+calib.stat <- function(p,cutpts,t,T.test,C.test,printTable=FALSE) {
   risk.class <- cut(p,cutpts,labels=FALSE)
   lev.stats <- sapply(1:(length(cutpts)-1),function(f) {
     ind <- which(risk.class==f)
     S.KM <- calcSurv(Surv(T.test[ind],C.test[ind]))
     ind.surv <- max(which(S.KM$t<=t))
     p.KM <- S.KM$SKM[ind.surv]
-    ##print(c(cutpts[f],p.KM,mean(p[ind],na.rm=TRUE),sqrt(S.KM$SKMV[ind.surv])))
+    
+    if(printTable) print(c(cutpts[f],p.KM,mean(p[ind],na.rm=TRUE),sqrt(S.KM$SKMV[ind.surv])))
+    
     (mean(p[ind],na.rm=TRUE) - p.KM)^2/S.KM$SKMV[ind.surv]
   })
   
