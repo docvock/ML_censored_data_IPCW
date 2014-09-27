868cd9a5e421f12438adf111e3bea2608a59d3e6
docvock <dmvock@gmail.com>
2014-09-27 18:45:05 -0500

    add additional ML techniques, edit code

--- code/IPCW_analysis.R
+++ code/IPCW_analysis.R
@@ -8,19 +8,19 @@ source("C:\\Users\\bstvock\\Documents\\research\\IPCW_machine_learning\\code\\IP
 
 # libraries used
 library(data.table)
-library(dplyr)
-#library(peperr)
-library(survey) 
-library(rpart)
-library(gam)
-library(earth)
-library(survival)
+library(dplyr) 
+library(survey) # for weighted generalized linear models
+library(rpart) # for recursive partitioning
+library(earth) # for multivariate addaptive regression splines
+library(mgcv) # for generalized additive models
+library(caret) # for k-nearest neighbors
+library(survival) # for Kaplan-Meier estimators
 
 #  Read the data
 HP.imp <- fread(paste0(dat.base,"Data_ver-2.3.1_imputed.csv"),verbose=TRUE)
 #  Restrict Dataset to those over 40 and with no comorbidites
 HP <- subset(HP.imp, age >= 40 & Comorbidity_All == 0) 
-#  Define factor variables and remove outlier for Total Cholesterol 
+#  Define factor variables, get standardized variabels, remove outlier for Total Cholesterol 
 HP <- mutate(HP, TC = pmin(TC, 500), gender_f = as.factor(gender),
 	Has_Diab_f = as.factor(Has_Diab),
 	Smoking_f = as.factor(Smoking),
@@ -28,7 +28,16 @@ HP <- mutate(HP, TC = pmin(TC, 500), gender_f = as.factor(gender),
 	gender_SBP_Meds = ifelse(gender==1 & SBP_Meds == 1, 1,
 															ifelse(gender == 0 & SBP_Meds == 1, 2,
 																ifelse(gender == 1 & SBP_Meds == 0, 3, 4))),
-																gender_SBP_Meds = as.factor(gender_SBP_Meds))
+	gender_s = stand(gender), 
+	age_s = stand(age),
+	SBP_s = stand(SBP),
+	BMI_s = stand(BMI),
+	HDL_s = stand(HDL),
+	TC_s = stand(TC),
+	Has_Diab_s = stand(Has_Diab),
+	Smoking_s = stand(Smoking),
+	SBP_Meds_s = stand(SBP_Meds))
+
 # Time horizon for prediction in years
 SURVTIME <- 7 
 # Define the outcome of interest (time to Hard CVD, Soft CVD, etc.) 
@@ -45,43 +54,132 @@ HP.train <- data.frame(HP[train.set,])
 HP.test <- data.frame(HP[test.set,])
 
 #  Features to use in analysis
-varnms <- c("gender_f","age","SBP","BMI","HDL","TC","Has_Diab_f","Smoking_f", "SBP_Meds_f")
-intx.varnms <- c(varnms,paste0("gender_f:",varnms[-1]))
+varnms <- c("gender_f", "age", "SBP", "BMI", "HDL", "TC", "Has_Diab_f", "Smoking_f", "SBP_Meds_f")
+varnms_a <- c("gender_f", "age", "SBP", "BMI", "HDL", "TC", "Has_Diab_f", "Smoking", "SBP_Meds_f")
+intx.varnms <- c(varnms,paste0("gender_f:",varnms[-1]),"SBP:SBP_Meds_f","gender_f:SBP:SBP_Meds_f")
+varnms_fixed <- c("gender_f", "Has_Diab_f", "Smoking_f", "SBP_Meds_f", "gender_f:Has_Diab_f", 
+	"gender_f:Smoking_f", "gender_f:SBP_Meds_f")
+varnms_smooth <- cbind(c("age", "SBP", "BMI", "HDL", "TC"),
+	c("gender_f", "gender_SBP_Meds", "gender_f", "gender_f", "gender_f"))
+varnms_stand <- c("gender_s", "age_s", "SBP_s", "BMI_s", "HDL_s", "TC_s", "Has_Diab_s", 
+	"Smoking_s", "SBP_Meds_s")
 
 # Get IPC Weights
 sf.C <- survfit(Surv(T.use, 1-C.use) ~ 1, data = HP.train)
 KM.C <- function(t) {
-    sf.C$surv[min(which(sf.C$time > t))]
+    KM.C <- sapply(1:length(t), function(pos) {
+    	sf.C$surv[min(which(sf.C$time > t[pos]))]
+    })
   }
 HP.train <- mutate(HP.train, wts = ifelse(T.use < (SURVTIME*365) & C.use==0, 0,
-	                           1 / KM.C(min(T.use , SURVTIME*365)) ))
+	                           1 / KM.C(pmin(T.use , SURVTIME*365)) ))
 
 
 #  Recursive Partitionin Models
-pRP <- rpart_functions(varnms, "IPCW", HP.train, HP.test, cp = 0.00001, minbucket = 100)
-pRP.zero <- rpart_functions(varnms, "ZERO", HP.train, HP.test, cp = 0.00001, minbucket = 100)
-pRP.disc <- rpart_functions(varnms, "DISCARD", HP.train, HP.test, cp = 0.00001, minbucket = 100)
+lossmat <- rbind(c(0,1),c(5,0))
+pRP <- rpart_functions(varnms_a, "IPCW", HP.train, HP.test, lossmat, "information", cp = 0.00001, 
+	minbucket = 100)
+pRP.zero <- rpart_functions(varnms_a, "ZERO", HP.train, HP.test, lossmat, "information", 
+	cp = 0.00001, minbucket = 100)
+pRP.disc <- rpart_functions(varnms_a, "DISCARD", HP.train, HP.test, lossmat, "information", 
+	cp = 0.00001, minbucket = 100)
+
+#  Bagged Recursive Partitionin Models
+lossmat <- rbind(c(0,1),c(5,0))
+set.seed(1101985)
+pRP.bag <- rpart_bag_functions(varnms_a, 50, "IPCW", HP.train, HP.test, lossmat, "information",
+	cp = 0.00001, minbucket = 100)
+pRP.bag.zero <- rpart_bag_functions(varnms_a, 50, "ZERO", HP.train, HP.test, lossmat, "information", 
+	cp = 0.00001, minbucket = 100)
+pRP.bag.disc <- rpart_functions(varnms_a, 50, "DISCARD", HP.train, HP.test, lossmat, "information", 
+	cp = 0.00001, minbucket = 100)
 
 #  Logistic Regression Models
-pLR <- logistic_functions(intx.varnms, HP.train, HP.test)
-pLR.zero <- logistic_functions(intx.varnms,HP.train, HP.test)
-pLR.disc <- logistic_functions(intx.varnms,HP.train, HP.test)
+pLR <- logistic_functions(intx.varnms, "IPCW", HP.train, HP.test)
+pLR.zero <- logistic_functions(intx.varnms, "ZERO", HP.train, HP.test)
+pLR.disc <- logistic_functions(intx.varnms, "DISCARD", HP.train, HP.test)
+
+#  Bagged Logistic Regression Models
+set.seed(1101985)
+pLR.bag <- logistic_bag_functions(intx.varnms, 50, "IPCW", HP.train, HP.test)
+pLR.bag.zero <- logistic_bag_functions(intx.varnms, 50, "ZERO", HP.train, HP.test)
+pLR.bag.disc <- logistic_bag_functions(intx.varnms, 50, "DISCARD", HP.train, HP.test)
 
 #  Generalized Additive Models
+pGAM <- GAM_functions(varnms_fixed, varnms_smooth, "IPCW", HP.train, HP.test)
+pGAM.zero <- GAM_functions(intx.varnms, "ZERO", HP.train, HP.test)
+pGAM.disc <- GAM_functions(intx.varnms, "DISCARD", HP.train, HP.test)
+
+#  Bagged Generalized Additive Models
+pGAM.bag <- GAM_bag_functions(varnms_fixed, varnms_smooth, 10, "IPCW", HP.train, 
+	HP.test)
 
 #  Bagged MARS Models
+set.seed(1101985)
+pMARS <- MARS_bag_functions(varnms, 50, "IPCW", HP.train, HP.test)
+pMARS.zero <- MARS_bag_functions(varnms, 50, "ZERO", HP.train, HP.test)
+pMARS.disc <- MARS_bag_functions(varnms, 50, "DISC", HP.train, HP.test)
 
-#  Bagged Recursive Partition Models
+#  Bagged knn
+set.seed(1101985)
+pknn <- knn_bag_functions(varnms_stand, 50, "IPCW", HP.train, HP.test, k = 100)
+pknn.zero <- knn_bag_functions(varnms_stand, 50, "ZERO", HP.train, HP.test, k = 100)
+pknn.disc <- knn_bag_functions(varnms_stand, 50, "DISC", HP.train, HP.test, k = 100)
 
 
+#  Calibration statistics
 risk.cutpts = c(Inf,1-c(0.05,0.1,0.15,0.2),-Inf)
 library(survMisc)
 
 calib.stat(pRP, risk.cutpts, HP.test)
-calib.stat(pRP.zero,risk.cutpts,SURVTIME*365,T.test,C.test)
-calib.stat(pRP.disc,risk.cutpts,SURVTIME*365,T.test,C.test)
-calib.stat(pLR,risk.cutpts,SURVTIME*365,T.test,C.test)
-calib.stat(pLR.zero,risk.cutpts,SURVTIME*365,T.test,C.test)
-calib.stat(pLR.disc,risk.cutpts,SURVTIME*365,T.test,C.test)
+calib.stat(pRP.zero, risk.cutpts, HP.test)
+calib.stat(pRP.disc, risk.cutpts, HP.test)
+
+calib.stat(pRP.bag, risk.cutpts, HP.test)
+calib.stat(pRP.bag.zero, risk.cutpts, HP.test)
+calib.stat(pRP.bag.disc, risk.cutpts, HP.test)
+
+calib.stat(pLR, risk.cutpts, HP.test)
+calib.stat(pLR.zero, risk.cutpts, HP.test)
+calib.stat(pLR.disc, risk.cutpts, HP.test)
+
+calib.stat(pLR.bag, risk.cutpts, HP.test)
+calib.stat(pLR.bag.zero, risk.cutpts, HP.test)
+calib.stat(pLR.bag.disc, risk.cutpts, HP.test)
+
+calib.stat(pGAM, risk.cutpts, HP.test)
+calib.stat(pGAM.zero, risk.cutpts, HP.test)
+calib.stat(pGAM.discard, risk.cutpts, HP.test)
+
+calib.stat(pGAM.bag, risk.cutpts, HP.test)
+calib.stat(pMARS, risk.cutpts, HP.test)
+calib.stat(pknn, risk.cutpts, HP.test)
+
+# Evaluate the C-index
+Cindex(pRP, HP.test)
+Cindex(pRP.zero, HP.test)
+Cindex(pRP.disc, HP.test)
+
+Cindex(pRP.bag, HP.test)
+Cindex(pRP.bag.zero, HP.test)
+Cindex(pRP.bag.disc, HP.test)
+
+Cindex(pLR, HP.test)
+Cindex(pLR.zero, HP.test)
+Cindex(pLR.disc, HP.test)
+
+Cindex(pLR.bag, HP.test)
+Cindex(pLR.bag.zero, HP.test)
+Cindex(pLR.bag.disc, HP.test)
+
+Cindex(pGAM, HP.test)
+Cindex(pGAM.zero, HP.test)
+Cindex(pGAM.disc, HP.test)
+
+Cindex(pGAM.bag, HP.test)
+Cindex(pMARS, HP.test)
+Cindex(pknn, HP.test)
 
 ## Evaluate the reclassification performance of well-calibrated models
+
+
--- code/IPCW_fns_complete.R
+++ code/IPCW_fns_complete.R
@@ -1,26 +1,71 @@
-rpart_functions <- function(varnms, cens_method = "IPCW", train.dat, test.dat = NULL,...) {  
+stand <- function(x){
+	stand <- (x-mean(x))/sd(x)
+}
+
+rpart_functions <- function(varnms, cens_method = "IPCW", train.dat, test.dat = NULL, 
+	lossmat = matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2), split = "information",...) {  
 
 	fmla <- as.formula(paste0("eventsurv.ind~",paste0(varnms,collapse="+")))
 	if(cens_method == "IPCW") {
-		RP <- rpart(fmla, data = train.dat, weights = wts, method="class", control=rpart.control(...))
+		RP <- rpart(fmla, data = train.dat, weights = wts, method="class", 
+			parms = list(loss = lossmat, split = split), control=rpart.control(...))
 	}
 	if(cens_method == "ZERO") {
-		RP <- rpart(fmla, data = train.dat, method = "class", control = rpart.control(...))
+		RP <- rpart(fmla, data = train.dat, method = "class", 
+			parms = list(loss = lossmat, split = split), control = rpart.control(...))
 	}
 	if(cens_method == "DISCARD") {
-		RP <- rpart(fmla, data = filter(train.dat, !(T.use <= (SURVTIME*365) & C.use == 0)), method = "class",
-			control=rpart.control(...))
+		RP <- rpart(fmla, data = filter(train.dat, !(T.use <= (SURVTIME*365) & C.use == 0)), 
+			method = "class", parms = list(loss = lossmat, split = split), control=rpart.control(...))
 	}	
   if(is.null(test.dat)) {
     probs <- predict(RP, type = "prob", na.action = na.omit)[,1]    
   } else {
     probs <- predict(RP, newdata = test.dat, type = "prob", na.action = na.omit)[,1]
-  }
+  }	
   return(probs)
 }
 
+# Recursive Partitioning Bagging Functions
+rpart_bag_functions <- function(varnms, B, cens_method = "IPCW", train.dat.all, test.dat = NULL,
+	lossmat = matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2), split = "information",...) { 
+
+	if(is.null(test.dat)) {
+		pred.prob <- matrix(0, nrow = nrow(train.dat.all), ncol = B)
+	} else {
+		pred.prob <- matrix(0, nrow = nrow(test.dat), ncol = B)	
+	}
+	fmla <- as.formula(paste0("eventsurv.ind~",paste0(varnms,collapse="+")))
+
+	for(q in 1:B) {
+		if(cens_method == "IPCW") {
+			train.dat <- sample_n(train.dat.all, nrow(train.dat.all), replace=TRUE, weight = wts)
+		}
+		if(cens_method == "ZERO") {
+			train.dat <- sample_n(train.dat.all, nrow(train.dat.all), replace=TRUE)
+		}
+		if(cens_method == "DISCARD") {
+			train.dat.disc <- filter(train.dat.all, !(T.use <= (SURVTIME*365) & C.use == 0))
+			train.dat <- sample_n(train.dat.disc, nrow(train.dat.all), replace=TRUE)
+		}
+		RP_B <- rpart(fmla, data = train.dat, method = "class", 
+			parms = list(loss = lossmat, split = split), control = rpart.control(...))
+		
+		if(is.null(test.dat)) {
+			pred.prob.B <- predict(RP_B, type = "prob", na.action=na.omit)[,1]    
+		} else {
+			pred.prob.B <- predict(RP_B, newdata=test.dat, type = "prob", na.action=na.omit)[,1]    
+  	}
+		pred.prob[, q] <- pred.prob.B
+		print(q)
+	}
+	probs <- apply(pred.prob, 1, mean)
+	return(probs)
+	}
+
 ## IPCW logisitc regression
-logistic_functions <- function(varnms, cens_method = "IPCW", train.dat, test.dat = NULL, ID = 1:nrow(train.dat),...) {  
+logistic_functions <- function(varnms, cens_method = "IPCW", train.dat, test.dat = NULL, 
+	ID = 1:nrow(train.dat),...) {  
 
   fmla <- as.formula(paste0("eventsurv.ind~",paste0(varnms,collapse="+")))
   
@@ -32,16 +77,193 @@ logistic_functions <- function(varnms, cens_method = "IPCW", train.dat, test.dat
 		LR <- glm(fmla,data=train.dat,family="binomial")
 	}
 	if (cens_method == "DISCARD") {
-		LR <- glm(fmla, data = filter(train.dat,!(T.use <= (SURVTIME*365) & C.use == 0)),family = "binomial")
+		LR <- glm(fmla, data = filter(train.dat,!(T.use <= (SURVTIME*365) & C.use == 0)), 
+			family = "binomial")
 	}
 	
   if(is.null(test.dat)) {
-    probs <- predict(LR, type = "response", na.action=na.omit)    
+    probs <- predict(LR, type = "response", na.action=na.omit)[,1]    
   } else {
-    probs <- predict(LR, newdata=test.dat, type = "response", na.action = na.omit)    
+    probs <- predict(LR, newdata=test.dat, type = "response", na.action = na.omit)[,1]    
   }
   return(1-probs)
 }
+
+# Logistic Bagging Functions
+logistic_bag_functions <- function(varnms, B, cens_method = "IPCW", train.dat.all, test.dat = NULL) { 
+
+	if(is.null(test.dat)) {
+		pred.prob <- matrix(0, nrow = nrow(train.dat.all), ncol = B)
+	} else {
+		pred.prob <- matrix(0, nrow = nrow(test.dat), ncol = B)	
+	}
+
+  fmla <- as.formula(paste0("eventsurv.ind~",paste0(varnms,collapse="+")))
+
+	for(q in 1:B) {
+		if(cens_method == "IPCW") {
+			train.dat <- sample_n(train.dat.all, nrow(train.dat.all), replace=TRUE, weight = wts)
+		}
+		if(cens_method == "ZERO") {
+			train.dat <- sample_n(train.dat.all, nrow(train.dat.all), replace=TRUE)
+		}
+		if(cens_method == "DISCARD") {
+			train.dat.disc <- filter(train.dat.all, !(T.use <= (SURVTIME*365) & C.use == 0))
+			train.dat <- sample_n(train.dat.disc, nrow(train.dat.all), replace=TRUE)
+		}
+		LR_B <- glm(fmla, data = train.dat,family = "binomial")
+		
+		if(is.null(test.dat)) {
+			pred.prob.B <- predict(LR_B, type = "response", na.action=na.omit)    
+		} else {
+			pred.prob.B <- predict(LR_B, newdata=test.dat, type = "response", na.action=na.omit)    
+  	}
+		pred.prob[, q] <- pred.prob.B
+		print(q)
+	}
+	probs <- apply(pred.prob, 1, mean)
+	return(1-probs)
+	}
+
+# IPCW Generalized Additive Model
+GAM_functions <- function(varnms_fixed, varnms_smooth, cens_method = "IPCW", train.dat, 
+	test.dat = NULL) { 
+	
+	quantile <- stats::quantile
+	train.dat$eventsurv <- ifelse(train.dat$eventsurv.ind==TRUE, 1, 0)
+	fmla <- as.formula(paste0(c(paste0("eventsurv~", paste0(varnms_fixed, collapse="+")),
+		paste0(paste("s(", varnms_smooth[, 1],", by = ", varnms_smooth[, 2], ")", sep = ""), 
+			collapse = "+")), collapse = "+"))
+	
+	if (cens_method == "IPCW") {
+		GAM <- gam(fmla, data = train.dat, weights = wts, family = binomial)
+	}	
+	if (cens_method == "ZERO") {
+		GAM <- gam(fmla, data = train.dat, family = binomial)
+	}	
+	if (cens_method == "DISCARD") {
+		GAM <- gam(fmla, data = filter(train.dat, !(T.use <= (SURVTIME*365) & C.use == 0)), 
+			family = binomial)
+	}
+	
+	 if(is.null(test.dat)) {
+    probs <- predict(GAM, type = "response", na.action = na.omit)    
+  } else {
+    probs <- predict(GAM, newdata=test.dat, type = "response", na.action = na.omit)    
+  }
+  return(1-probs)
+}
+
+# Generalized Additive Model Bagging Functions
+GAM_bag_functions <- function(varnms_fixed, varnms_smooth, B, cens_method = "IPCW", train.dat.all,
+	test.dat = NULL) { 
+
+	if(is.null(test.dat)) {
+		pred.prob <- matrix(0, nrow = nrow(train.dat.all), ncol = B)
+	} else {
+		pred.prob <- matrix(0, nrow = nrow(test.dat), ncol = B)	
+	}
+	quantile <- stats::quantile
+	train.dat.all$eventsurv <- ifelse(train.dat.all$eventsurv.ind==TRUE, 1, 0)
+	fmla <- as.formula(paste0(c(paste0("eventsurv~", paste0(varnms_fixed, collapse="+")),
+		paste0(paste("s(", varnms_smooth[, 1],", by = ", varnms_smooth[, 2], ")", sep = ""), 
+			collapse = "+")), collapse = "+"))
+
+	for(q in 1:B) {
+		if(cens_method == "IPCW") {
+			train.dat <- sample_n(train.dat.all, nrow(train.dat.all), replace=TRUE, weight = wts)
+		}
+		if(cens_method == "ZERO") {
+			train.dat <- sample_n(train.dat.all, nrow(train.dat.all), replace=TRUE)
+		}
+		if(cens_method == "DISCARD") {
+			train.dat.disc <- filter(train.dat.all, !(T.use <= (SURVTIME*365) & C.use == 0))
+			train.dat <- sample_n(train.dat.disc, nrow(train.dat.all), replace=TRUE)
+		}
+		GAM_B <- gam(fmla, data = train.dat, family = binomial)
+		
+		if(is.null(test.dat)) {
+			pred.prob.B <- predict(GAM_B, type = "response", na.action=na.omit)    
+		} else {
+			pred.prob.B <- predict(GAM_B, newdata=test.dat, type = "response", na.action=na.omit)    
+  	}
+		pred.prob[, q] <- pred.prob.B
+		print(q)
+	}
+	probs <- apply(pred.prob, 1, mean)
+	return(1-probs)
+	}
+
+# MARS Bagging Functions
+MARS_bag_functions <- function(varnms, B, cens_method = "IPCW", train.dat.all, test.dat = NULL) { 
+
+	if(is.null(test.dat)) {
+		pred.prob <- matrix(0, nrow = nrow(train.dat.all), ncol = B)
+	} else {
+		pred.prob <- matrix(0, nrow = nrow(test.dat), ncol = B)	
+	}
+	train.dat.all$eventsurv <- ifelse(train.dat.all$eventsurv.ind==TRUE, 1, 0)
+  fmla <- as.formula(paste0("eventsurv~",paste0(varnms,collapse="+")))
+
+	for(q in 1:B) {
+		if(cens_method == "IPCW") {
+			train.dat <- sample_n(train.dat.all, nrow(train.dat.all), replace=TRUE, weight = wts)
+		}
+		if(cens_method == "ZERO") {
+			train.dat <- sample_n(train.dat.all, nrow(train.dat.all), replace=TRUE)
+		}
+		if(cens_method == "DISCARD") {
+			train.dat.disc <- filter(train.dat.all, !(T.use <= (SURVTIME*365) & C.use == 0))
+			train.dat <- sample_n(train.dat.disc, nrow(train.dat.all), replace=TRUE)
+		}
+		MARS_B <- earth(fmla, glm=list(family=binomial), nk = 50, degree = 3, thresh = 0.0005, 
+				data=train.dat)
+		if(is.null(test.dat)) {
+			pred.prob.B <- predict(MARS_B, type = "response")    
+		} else {
+			pred.prob.B <- predict(MARS_B, newdata=test.dat, type = "response")    
+  	}
+		pred.prob[, q] <- pred.prob.B
+		print(q)
+	}
+	probs <- apply(pred.prob, 1, mean)
+	return(1-probs)
+	}
+
+# kNN Bagging Functions
+knn_bag_functions <- function(varnms, B, cens_method = "IPCW", train.dat.all, test.dat = NULL,...) { 
+
+	if(is.null(test.dat)) {
+		pred.prob <- matrix(0, nrow = nrow(train.dat.all), ncol = B)
+	} else {
+		pred.prob <- matrix(0, nrow = nrow(test.dat), ncol = B)	
+	}
+  fmla <- as.formula(paste0("eventsurv.ind~",paste0(varnms,collapse="+")))
+
+	for(q in 1:B) {
+		if(cens_method == "IPCW") {
+			train.dat <- sample_n(train.dat.all, nrow(train.dat.all), replace=TRUE, weight = wts)
+		}
+		if(cens_method == "ZERO") {
+			train.dat <- sample_n(train.dat.all, nrow(train.dat.all), replace=TRUE)
+		}
+		if(cens_method == "DISCARD") {
+			train.dat.disc <- filter(train.dat.all, !(T.use <= (SURVTIME*365) & C.use == 0))
+			train.dat <- sample_n(train.dat.disc, nrow(train.dat.all), replace=TRUE)
+		}
+		knn_B <- knn3(fmla,  data=train.dat,...)
+		if(is.null(test.dat)) {
+			pred.prob.B <- predict(knn_B, type = "prob")[, 2]    
+		} else {
+			pred.prob.B <- predict(knn_B, newdata=test.dat, type = "prob")[, 2]    
+  	}
+		pred.prob[, q] <- pred.prob.B
+		print(q)
+	}
+	probs <- apply(pred.prob, 1, mean)
+	return(1-probs)
+	}
+
 
 ## Define the calibration statistic
 calib.stat <- function(p, cutpts, test.dat) {
@@ -60,7 +282,7 @@ calib.stat <- function(p, cutpts, test.dat) {
 }
 
 Cindex <- function(p, test.dat) {
-	test.dat$p <- p
+	test.dat$p <- 1-p
 	Cindex_all <- survConcordance(Surv(T.use, C.use) ~ p, data = test.dat)
 	Cindex <- Cindex_all$concordance
 	return(Cindex)
