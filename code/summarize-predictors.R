2fc36067374e6bf561f6d1fb866b4a8fdee71cf1
Julian Wolfson <julianw@umn.edu>
2014-09-29 00:36:27 -0500

    First version of code
    Code to summarize the predictors in Table 1

---
+++ code/summarize-predictors.R
@@ -0,0 +1,41 @@
+code.base <- "C:/Users/Julian/Google Drive/iPredict/manuscripts/ML-for-censored-data/code/"
+dat.base <- "H:/Carlson/Portal/Data/DataLock/DataLockv2.3.1/"
+
+library(data.table)
+## Read the data
+#HP <- fread(paste0(dat.base,"Data_ver-2.3.1_updated.csv"),verbose=TRUE)
+
+HP.imp <- fread(paste0(dat.base,"Data_ver-2.3.1_imputed.csv"),verbose=TRUE)
+HP <- subset(HP.imp,age>=40 & Comorbidity_All==0) ## Only those: Over age 40, no comorbidities
+
+frac.train <- 0.75
+train.set <- sample(1:nrow(HP),frac.train*nrow(HP),replace=FALSE)
+test.set <- setdiff(1:nrow(HP),train.set)
+
+HP.train <- data.frame(HP[train.set,])
+HP.test <- data.frame(HP[test.set,])
+
+T.train <- HP.train$DaysToEvent_Fram
+C.train <- HP.train$CVDEvent_Fram
+
+T.test <- HP.test$DaysToEvent_Fram
+C.test <- HP.test$CVDEvent_Fram
+
+## % censored prior to five years
+mn.cens <- mean(C.train==0 & T.train <= (5*365))
+print(mn.cens)
+
+## Number evaluated in discard
+sum.uncens <- sum(!(C.train==0 & T.train <= (5*365)))
+print(sum.uncens)
+
+### Get numbers to tabulate the predictors
+summary(HP)
+table(HP$gender)
+table(HP$gender)/nrow(HP)
+table(HP$Smoking)
+table(HP$Smoking)/nrow(HP)
+table(HP$SBP_Meds)
+table(HP$SBP_Meds)/nrow(HP)
+table(HP$Has_Diab)
+table(HP$Has_Diab)/nrow(HP)
